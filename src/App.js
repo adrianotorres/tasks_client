92bbf14c84028f7842f22b03eeb628896287493f
Adriano Torres <adriano.skilo@gmail.com>
2020-03-26 17:58:58 -0300

    Adding Tasks component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import Header from './components/header/Header';
+import Tasks from './components/tasks/Tasks';
 import './App.scss';
 
 library.add(faCheckCircle, faTrashAlt)
@@ -11,6 +13,9 @@ class App extends Component {
     return (
       <div>
         <Header/>
+        <Container>
+          <Tasks/>
+        </Container>
       </div>
     )
   };
