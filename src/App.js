dcebf633a91cd0818cb5d006faf73653f1cee2cb
Adriano Torres <adriano.skilo@gmail.com>
2020-03-26 17:54:33 -0300

    Adding Header component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCheckCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header/Header';
+import './App.scss';
 
 library.add(faCheckCircle, faTrashAlt)
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header/>
       </div>
     )
   };
