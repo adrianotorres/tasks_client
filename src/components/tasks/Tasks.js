6cd5a6f6656cdfdaaffadab64b6f5739f15b4374
Adriano Torres <adriano.skilo@gmail.com>
2020-03-28 16:44:35 -0300

    Remove all done tasks

--- src/components/tasks/Tasks.js
+++ src/components/tasks/Tasks.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import List from './list/List';
 import CreateTask from './create_tasks/CreateTasks';
 
@@ -12,12 +13,23 @@ class Tasks extends Component {
       tasks: []
     };
     this.loadTasks = this.loadTasks.bind(this);
+    this.removeAll = this.removeAll.bind(this);
   }
 
   async loadTasks() {
     let response = await fetch(`http://localhost:3001/tasks`);
     const tasks = await response.json();
     this.setState({ tasks: tasks });
+  }
+
+  async removeAll() {
+    if (window.confirm('Are you sure you want to delete all done tasks ?')) {
+      const promises = this.state.tasks.filter(task => task.done).map(async task =>
+        await fetch(`http://localhost:3001/tasks/${task.id}`, {method: 'DELETE'})
+      );
+      await Promise.all(promises)
+      this.loadTasks();
+    }
   }
 
   componentDidMount() {
@@ -29,13 +41,15 @@ class Tasks extends Component {
       <Row>
         <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
           <p className="title">To-do</p>
-          <List loadTasks={this.loadTasks} tasks={this.state.tasks.filter((task) => !task.done)}/>
+          <List loadTasks={this.loadTasks} tasks={this.state.tasks.filter(task => !task.done)}/>
           <CreateTask loadTasks={this.loadTasks}/>
         </Col>
         <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
           <p className="title">Done</p>
-          <List loadTasks={this.loadTasks} tasks={this.state.tasks.filter((task) => task.done)}/>
-          <Button variant="red" className="float-right remove_tasks_btn">Remove all tasks</Button>
+          <List loadTasks={this.loadTasks} tasks={this.state.tasks.filter(task => task.done)}/>
+          <Button variant="red" className="float-right remove_tasks_btn" onClick={() => this.removeAll()}>
+            <FontAwesomeIcon icon="trash-alt"/> Remove all done tasks
+          </Button>
         </Col>
       </Row>
     );
